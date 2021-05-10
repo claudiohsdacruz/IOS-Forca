cc3c56c99c87e99b6f52042bcad495c17f8d6714
Claudio Cruz <claudio.dacruz@icloud.com>
2021-05-10 01:02:52 -0300

    initial commit

--- forca/main.swift
+++ forca/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+
+var sorteada = Palavra()
+var jogo = Forca(palavra:sorteada.getPalavra(), dica: sorteada.getDica())
+
+while (!jogo.terminou()){
+    print("Dica: \(jogo.dica)")
+    print("Status: \(String(jogo.status))")
+    print("Letras: \(jogo.letrasJogadas)")
+    print("Tentativas restantes: \(10 - jogo.tentativas)")
+    
+    print("Digite uma letra: ")
+    let letra = Character(readLine()!)
+    jogo.jogar(letra: letra)
+}
+
+print(jogo.resultado())
 
